d4e336e6f163954cd31e6de29ba70a7e3d18b58f
pradnya-30 <73632699+pradnya-30@users.noreply.github.com>
2022-03-04 14:44:47 +0530

    initial commit of math.py

---
+++ math.py
@@ -0,0 +1,21 @@
+# ADD FUNCTION 
+def add(a,b):
+return (x+y)
+
+#subtract function
+def subtract(a,b):
+return(x-y)
+
+ 
+# MULTIPLY FUNCTION 
+def multiply(a,b):
+ pass
+
+# DIVIDE FUNCTION 
+def divide(a,b):
+
+ if(b==0):
+   print('divide by 0 error')
+ else:
+   return(a/b)
+
