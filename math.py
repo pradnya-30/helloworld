94e860e67f888d988fc690e6c5c35c4289840d81
pradnya <pradnya_gurav@persistent.com>
2022-03-04 14:56:21 +0530

    added implementation

--- math.py
+++ math.py
@@ -1,21 +1,19 @@
 # ADD FUNCTION 
-def add(a,b):
+def add(x,y):
 return (x+y)
 
 #subtract function
-def subtract(a,b):
+def subtract(x,y):
 return(x-y)
 
  
 # MULTIPLY FUNCTION 
 def multiply(a,b):
- pass
+ return(a*b)
 
 # DIVIDE FUNCTION 
 def divide(a,b):
-
  if(b==0):
    print('divide by 0 error')
  else:
    return(a/b)
-
